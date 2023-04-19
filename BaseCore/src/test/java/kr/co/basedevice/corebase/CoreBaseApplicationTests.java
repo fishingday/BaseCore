70fbe81879c15dc5d7c74075096bec2fb3a48e57
fishingday <fishingday@P00A80461-001.CNS.LGCNS.COM>
2023-04-19 20:15:33 +0900

    패스워드 테스트 코드 주가

--- BaseCore/src/test/java/kr/co/basedevice/corebase/CoreBaseApplicationTests.java
+++ BaseCore/src/test/java/kr/co/basedevice/corebase/CoreBaseApplicationTests.java
@@ -1,13 +1,26 @@
 package kr.co.basedevice.corebase;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
-class BaseDeviceApplicationTests {
+class CoreBaseApplicationTests {
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
     @Test
-    void contextLoads() {
+    void metchPwd() {
+    	String pwd = passwordEncoder.encode("1111"); //"$2a$10$ITvL7uD4IV65bzre4IKy7u8tlzmJUmnFdIqpEIT9SYempGwHEWZdq";
+    	
+    	System.err.println("###########[" + pwd + "]"); 
+    	
+    	// {bcrypt}$2a$10$1qiyUUZzWWG8PZRAL.BH..qQdWPR1DIOxxjt97ZlsyudyrzMfmK/K
+    	// {bcrypt}$2a$10$ErKoq2mRDOVizhm8MDqRnebaDfqRwU47ixtZM58aEIMbdJX3jjxYy
+    	
+    	System.err.println("###########[" + passwordEncoder.matches("1111", pwd) + "]");	
+    	
     }
-
 }
