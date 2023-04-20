0332479db21b42b470cb50fbc96e2835ce25c45f
fishingday <fishingday@P00A80461-001.CNS.LGCNS.COM>
2023-04-20 14:35:39 +0900

    양방향 암호화 테스트

--- BaseCore/src/test/java/kr/co/basedevice/corebase/CoreBaseApplicationTests.java
+++ BaseCore/src/test/java/kr/co/basedevice/corebase/CoreBaseApplicationTests.java
@@ -1,5 +1,18 @@
 package kr.co.basedevice.corebase;
 
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,5 +35,26 @@ class CoreBaseApplicationTests {
     	
     	System.err.println("###########[" + passwordEncoder.matches("1111", pwd) + "]");	
     	
+    }
+    
+    @Test
+    void testEnCypto() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
+	    // https://bamdule.tistory.com/234
+    	
+    	String alg = "AES/CBC/PKCS5Padding";
+    	String key = "01234567890123456789012345678901";
+    	String iv = key.substring(0, 16); // 16byte
+    	
+    	String text = "ASC256";
+    	
+    	 Cipher cipher = Cipher.getInstance(alg);
+         SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+         IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
+         cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParamSpec);
+
+         byte[] encrypted = cipher.doFinal(text.getBytes("UTF-8"));
+         String outText = Base64.getEncoder().encodeToString(encrypted);
+         
+         System.err.println("##################" + outText);
     }
 }
