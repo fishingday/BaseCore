4c25d3e3a7226d78f6b5360c19d1267ffaa7bba6
fishingday <fishingday@P00A80461-001.CNS.LGCNS.COM>
2023-07-11 16:22:20 +0900

    일단 기본 화면은 완료

--- BaseCore/src/main/java/kr/co/basedevice/corebase/controller/user/UserController.java
+++ BaseCore/src/main/java/kr/co/basedevice/corebase/controller/user/UserController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import kr.co.basedevice.corebase.domain.cm.CmUser;
-
 @Controller
 @RequestMapping("/user")
 public class UserController {
@@ -16,13 +14,7 @@ public class UserController {
 	}
 	
 	@GetMapping(value={"/bookmark", "/bookmark/init.html"})
-	public String viewBookmark() throws Exception {
-		CmUser user = null;
-		
-		if(true) {
-			user.getCreDt();
-		}
-		
+	public String viewBookmark() throws Exception {		
 		return "/user/bookmark/bookmark.html";
 	}
 	
