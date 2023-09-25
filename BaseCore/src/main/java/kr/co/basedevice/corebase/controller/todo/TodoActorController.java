5a2a3fe88d80ab1489359d97960f8a360ca08d40
fishingday <fishingday@P00A80461-001.CNS.LGCNS.COM>
2023-09-25 19:50:45 +0900

    오류 쩐다~

---
+++ BaseCore/src/main/java/kr/co/basedevice/corebase/controller/todo/TodoActorController.java
@@ -0,0 +1,5 @@
+package kr.co.basedevice.corebase.controller.todo;
+
+public class TodoActorController {
+
+}
---
+++ BaseCore/src/main/java/kr/co/basedevice/corebase/restController/todo/ChoiceTodoItemRestController.java
@@ -0,0 +1,5 @@
+package kr.co.basedevice.corebase.restController.todo;
+
+public class ChoiceTodoItemRestController {
+
+}
