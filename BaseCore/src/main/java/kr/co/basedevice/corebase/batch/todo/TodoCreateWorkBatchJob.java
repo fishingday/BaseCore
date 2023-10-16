8f75d0cd466dafbe059583e2c70440f1d8b9bb8a
fishingday <fishingday@gmail.com>
2023-10-17 01:06:15 +0900

    아우...

--- BaseCore/src/main/java/kr/co/basedevice/corebase/batch/todo/TodoCreateWorkBatchJob.java
+++ BaseCore/src/main/java/kr/co/basedevice/corebase/batch/todo/TodoCreateWorkBatchJob.java
@@ -1,5 +1,7 @@
 package kr.co.basedevice.corebase.batch.todo;
 
+import java.util.List;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
@@ -7,14 +9,26 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import kr.co.basedevice.corebase.domain.td.TdTodo;
 import kr.co.basedevice.corebase.quartz.job.TodoCreateWorkJob;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+
+
+/**
+ * 기본 Tasklet 기반의 Batch Job
+ * step1이 데이터를 조회하고,
+ * step2가 데이터를 기록한다.
+ * 
+ * @author fishingday
+ *
+ */
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
@@ -22,8 +36,7 @@ public class TodoCreateWorkBatchJob {
 	final private JobBuilderFactory jobBuilderFactory;
 	final private StepBuilderFactory stepBuilderFactory;
 
-
-    @Bean(name = TodoCreateWorkJob.JOB_NAME)
+	@Bean(name = TodoCreateWorkJob.JOB_NAME)
     Job createWorkTodoBatchJob(){
         return jobBuilderFactory.get(TodoCreateWorkJob.JOB_NAME)
                 .start(step1CreateWorkBatchTodoJob()) // 할일 조회
@@ -36,8 +49,15 @@ public class TodoCreateWorkBatchJob {
         return stepBuilderFactory.get("step1CreateWorkBatchTodoJob")
                 .tasklet(new Tasklet() {
                     @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        log.info("########## createJob1_Step1 start!!!!");
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext context) throws Exception {
+                        ExecutionContext jobContext = context.getStepContext().getStepExecution().getJobExecution().getExecutionContext();
+                        
+                        
+                        List<TdTodo> tdTodoList = null;
+                        
+                        
+                        jobContext.put("tdTodoList", tdTodoList);
+                    	
                         return RepeatStatus.FINISHED;
                     }
                 }).build();
